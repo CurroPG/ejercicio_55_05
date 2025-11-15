3f36a911de6fa276342634cd41289125602d99cf
Curro Portillo <211curroportillo@gmail.com>
2025-11-15 12:46:20 +0100

    ejercicio 55

--- src/App.java
+++ src/App.java
@@ -1,5 +1,31 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numero = Integer.parseInt(System.console().readLine("Introduzca un número: "));
+        int aux = numero;
+        aux = aux * 10 + 1;
+        int digito = 0;
+        int invertido = 0;
+        int ultimoDigito = numero % 10;
+
+        do{
+            digito = aux % 10;
+            invertido = invertido * 10 + digito;
+            aux/=10;
+        }while(aux != 0);
+
+        digito = 0;
+        aux = invertido;
+        invertido = 0;
+        invertido = invertido * 10 + ultimoDigito;
+
+        do{
+            if(aux != 1){
+                digito = aux % 10;
+                invertido = invertido * 10 + digito;
+            }
+            aux/=10;
+        }while(aux != 1);
+        invertido = invertido / 10;
+        System.out.printf("El número resultado es %d", invertido);
     }
 }
